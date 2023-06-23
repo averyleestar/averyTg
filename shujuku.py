48660ac2468dd583dca77ae76c327f63dd4ac9be
avery <tongxue202@gmail.com>
2023-06-23 12:11:25 +0800

    测试

--- shujuku.py
+++ shujuku.py
@@ -1,12 +1,12 @@
 import pymysql
-
+import MySQLdb
 def mysql_db():
     # 连接数据库的参数
     conn = pymysql.connect(
             host="47.242.62.208", port='3309', database = "tgxe",
                       charset= "utf8", user = "tgxe",  passwd= "55GBLMkwfNBdaRRm")
-if __name__ == "__main__":
-    mysql_db()
+# if __name__ == "__main__":
+#     mysql_db()
 
 # class conf():
 #     def con(config):
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 #                       "charset": "utf8", "user": "tgxe", "passwd": "55GBLMkwfNBdaRRm"}
 #         return config
 
-    try:
-        with conn.cursor() as cursor:
-            sql = select *
-            from user
+
+class MySQLdb():
+    def __init__(self):
+        self.conn = self.get__conn()
